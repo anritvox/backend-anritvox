92c4b5af5ef9cbc4674ff87f6fc3c62df657a7a9
anritvox <anritvox@gmail.com>
2026-04-01 18:25:33 +0530

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,4 +1,5 @@
 // backend/routes/contactRoutes
+
 const express = require("express");
 const router = express.Router();
 const { getAllMessages, createMessage } = require("../models/contactModel");
