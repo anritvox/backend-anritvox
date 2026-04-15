b122d504f744197bad2b08cec2ee8aaba701bc54
anritvox <anritvox@gmail.com>
2026-04-15 12:56:46 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const pool = require('../config/db');
 
 // Import BOTH Admin and Customer Models (Aliasing verifyPassword to prevent conflicts)
