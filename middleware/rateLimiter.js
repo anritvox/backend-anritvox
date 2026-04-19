8f8a8cd1af9eef9e10e94a7c4804a0ba2234d6ae
anritvox <anritvox@gmail.com>
2026-04-19 11:34:14 +0530

    Create rateLimiter.js

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,28 @@
+const rateLimit = require('express-rate-limit');
+
+
+const registerLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, 
+    max: 3,
+    message: {
+        success: false,
+        message: "Too many accounts created from this IP. Please try again after an hour."
+    },
+    standardHeaders: true,
+    legacyHeaders: false, 
+});
+
+
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 5,
+    message: {
+        success: false,
+        message: "Too many login attempts from this IP. Please try again after 15 minutes."
+    }
+});
+
+module.exports = {
+    registerLimiter,
+    loginLimiter
+};
