a30b89fdcd8acf09586a565e4952e86a94f54ffe
anritvox <anritvox@gmail.com>
2026-07-23 13:59:10 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,28 +1,13 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+const mysql = require('mysql2/promise');
 
-const dbConfig = {
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 10000,
-};
-
-if (process.env.NODE_ENV === "production") {
-  dbConfig.ssl = {
-    rejectUnauthorized: false,
-  };
-} else if (process.env.DB_SSL_ENABLED === "true") {
-  dbConfig.ssl = {
-    rejectUnauthorized: false,
-  };
-}
-
-const pool = mysql.createPool(dbConfig).promise();
+  connectionLimit: 3, 
+});
 
 module.exports = pool;
